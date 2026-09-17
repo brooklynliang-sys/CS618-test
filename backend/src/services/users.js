92f8e59ef5a6f559dd963bb032c48c044eb9da47
YongLiang_CS690 <brooklynliang@gmail.com>
2026-09-17 03:59:33 +0000

    feat: add user authentication and JWT middleware

--- backend/src/services/users.js
+++ backend/src/services/users.js
@@ -1,11 +1,13 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { User } from '../db/models/user.js'
+
 export async function createUser({ username, password }) {
   const hashedPassword = await bcrypt.hash(password, 10)
   const user = new User({ username, password: hashedPassword })
   return await user.save()
 }
+
 export async function loginUser({ username, password }) {
   const user = await User.findOne({ username })
   if (!user) {
@@ -19,4 +21,14 @@ export async function loginUser({ username, password }) {
     expiresIn: '24h',
   })
   return token
+}
+
+export async function getUserInfoById(userId) {
+  try {
+    const user = await User.findById(userId)
+    if (!user) return { username: userId }
+    return { username: user.username }
+  } catch (err) {
+    return { username: userId }
+  }
 }
